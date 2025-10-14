413a859a8fb8b6822486dd684d5caa1028a9a71d
C.E. Carmine <->
2025-10-14 16:38:22 -0400

    Add dev module

---
+++ modules/dev/module.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  programs.steam.enable = true;
+
+  users.users.carmine = {
+    packages = with pkgs; [
+      jetbrains.idea-community-bin
+      rstudio
+      vscode
+      vscode-extensions.jnoortheen.nix-ide
+      vscode-extensions.llvm-vs-code-extensions.vscode-clangd
+      vscode-extensions.redhat.vscode-yaml
+      vscode-extensions.redhat.vscode-xml
+      vscode-extensions.rust-lang.rust-analyzer
+      vscode-extensions.tamasfe.even-better-toml
+    ];
+  };
+}
