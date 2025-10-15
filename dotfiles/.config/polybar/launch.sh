4158d51d4a3ef03e0db527e272885a3991234ffd
C.E. Carmine <jupiterisopod@gmail.com>
2025-10-15 00:36:15 -0400

    Fix polybar launch script

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 CONFIG="$HOME/.config/polybar/config.ini"
 
-# Terminate already running bars
+# Terminate already running bar instances
 killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-polybar -q main -c "$CONFIG"
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar -q main -c "$CONFIG" &
